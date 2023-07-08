a87446b103d41a71f644d593edf2b733043373a8
Shoxrux Asadov <129892167+ShoxruxAsadov@users.noreply.github.com>
2023-07-08 16:50:17 +0500

    Fifth Data

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,56 @@
+import { Link } from "react-router-dom";
+
+import {
+  BiLogoInstagramAlt,
+  BiLogoLinkedin,
+  BiLogoGithub,
+  BiSolidCloudDownload,
+} from "react-icons/bi";
+
 export default function Footer() {
   return (
     <footer id="contact">
       <div className="container">
-          <h1>Footer</h1>n
+        <div className="footer-top">
+          <div className="social"></div>
         </div>
+        <div className="footer-bottom">
+          <div className="social">
+            <Link
+              to={"https://www.instagram.com/Shokh.dev"}
+              className="social instagram"
+              target="_blank"
+            >
+              <BiLogoInstagramAlt /> <span>Instagram</span>
+            </Link>
+            <Link
+              to={"https://github.com/ShoxruxAsadov"}
+              className="social github"
+              target="_blank"
+            >
+              <BiLogoGithub /> <span>GitHub</span>
+            </Link>
+            <Link
+              to={"https://www.linkedin.com/in/shoxruxasadov/"}
+              className="social linkedin"
+              target="_blank"
+            >
+              <BiLogoLinkedin /> <span>LinkedIn</span>
+            </Link>
+            <Link
+              className="social resume"
+              to={"./resume.pdf"}
+              download="Resume"
+              target="_blank"
+            >
+              <BiSolidCloudDownload /> <span>Resume</span>
+            </Link>
+          </div>
+          <div className="ooo">
+            <p>Shoxrux Asadov © 2022-2024.</p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
