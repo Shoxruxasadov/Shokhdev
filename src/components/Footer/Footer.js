b34645a214bd61b7612d2adaf2de78cd4f0fc570
Shoxrux Asadov <129892167+ShoxruxAsadov@users.noreply.github.com>
2023-07-08 17:16:42 +0500

    Newing Data

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,12 +7,33 @@ import {
   BiSolidCloudDownload,
 } from "react-icons/bi";
 
+import { IoCall, IoLocationSharp } from "react-icons/io5";
+import { IoMdMailUnread } from "react-icons/io";
+
 export default function Footer() {
   return (
     <footer id="contact">
       <div className="container">
+        <h1 className="LetsWork">
+          Have a Project? <br /> Let's Work Together
+        </h1>
         <div className="footer-top">
-          <div className="social"></div>
+          <div className="contacts">
+            <div className="contact calllock">
+              <div className="contact call">
+                <IoCall />
+                <span>+998 91 415 78 33</span>
+              </div>
+              <div className="contact location">
+                <IoLocationSharp />
+                <span>Tashkent, Uzbekistan</span>
+              </div>
+            </div>
+            <div className="contact mail">
+              <IoMdMailUnread />
+              <span>Shoxruxasadov05@mail.ru</span>
+            </div>
+          </div>
         </div>
         <div className="footer-bottom">
           <div className="social">
